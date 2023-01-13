59d820a4dd5b5ab591b24c7bf9a96b0213976b5b
YuanchengFang <ridiculous46@outlook.com>
2023-01-13 22:59:21 +0800

    add

--- README.md
+++ README.md
@@ -18,7 +18,30 @@ Run with SIFT bag of words method
 python main.py --method sift
 ```
 
+Analyze the relation between vocabulary size and performance
+
+```shell
+python main.py --analyze vocab
+```
+
+Analyze performance on different categories (SIFT)
+
+```shell
+python main.py --analyze category
+```
+
+Analyze performance on different categories (Tiny Images)
+
+```shell
+python main.py --analyze tiny
+```
+
+
+
+`plot.ipynb` is used to plot images I use in my report.
+
 Note
 ----
-- Train data and test data are in `data.zip`
-- You may need to change the `TRAIN_DIR` and `TEST_DIR` in `main.py`, to specify input directory.
+
+- Input train data and test data are in `data.zip`
+- May need to change the `TRAIN_DIR` and `TEST_DIR` in `main.py`, to specify input directory.
--- main.py
+++ main.py
@@ -17,6 +17,9 @@ TEST_DIR = './test'
 # NOTE: directory to store kmeans model
 MODEL_DIR = './models'
 
+# NOTE:
+RESULTS_DIR = './results'
+
 
 def tiny_classifier():
     features, labels = load_tiny_features(TRAIN_DIR)
@@ -29,24 +32,25 @@ def evaluate_tiny(clf):
     X, y = load_tiny_features(TEST_DIR)
     y_pred = clf.predict(X)
     accuracy = (y_pred == y).sum() / y.shape[0]
-    return accuracy
+    return accuracy, y_pred, y
 
 
-def sift_cluster_classifer(n_clusters=64):
-    features, labels = load_sift_features(TRAIN_DIR)
-    vocab = load_vocab(features)
+def sift_cluster_classifer(features, labels, n_clusters=64, vocab_size=64000):
+    vocab = load_vocab(features, vocab_size=vocab_size)
 
-    model_path = os.path.join(MODEL_DIR, 'kmeans_sift.joblib')
+    model_path = os.path.join(MODEL_DIR, 'kmeans_%d.joblib'%vocab_size)
     if os.path.exists(model_path):
         print('Load kmeans models from', model_path)
         cluster = joblib.load(model_path)
     else:
         print('Training KMeans cluster...')
         cluster = KMeans(n_clusters=n_clusters).fit(vocab)
+        if not os.path.exists(MODEL_DIR):
+            os.mkdir(MODEL_DIR)
         joblib.dump(cluster, model_path)
     
     X = []
-    for des in tqdm(features, desc='Generate histograms'):
+    for des in tqdm(features, desc='Generate train histograms'):
         pred = cluster.predict(des)
         # bincount
         count = np.bincount(pred, minlength=64)
@@ -60,10 +64,9 @@ def sift_cluster_classifer(n_clusters=64):
     return cluster, clf
 
 
-def evaluate_sift(cluster, clf):
-    features, y = load_sift_features(TEST_DIR)
+def evaluate_sift(features, labels, cluster, clf):
     X = []
-    for des in tqdm(features, desc='Generate histograms'):
+    for des in tqdm(features, desc='Generate test histograms'):
         pred = cluster.predict(des)
         # bincount
         count = np.bincount(pred, minlength=64)
@@ -73,23 +76,105 @@ def evaluate_sift(cluster, clf):
     X = np.stack(X, axis=0)
 
     y_pred = clf.predict(X)
-    accuracy = (y_pred == y).sum() / y.shape[0]
-    return accuracy
+    accuracy = (y_pred == labels).sum() / y_pred.shape[0]
+    return accuracy, y_pred
+
+
+def analyze_vocabsize():
+    test_sizes = np.int32([16, 32, 64, 96]) * 1000
+    # test_sizes = np.int32([16, 32, 40, 50, 64, 70, 80, 96]) * 1000
+
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
+    with open(os.path.join(RESULTS_DIR, 'vocab.csv'), 'w', encoding='utf-8') as f:
+        f.write('Vocabsize\tAccuracy\n')
+    
+    features, labels = load_sift_features(TRAIN_DIR)
+    features_test, labels_test = load_sift_features(TEST_DIR)
+    for vocab_size in test_sizes:
+        cluster, clf = sift_cluster_classifer(features, labels, vocab_size=vocab_size)
+        acc, _ = evaluate_sift(features_test, labels_test, cluster, clf)
+        with open(os.path.join(RESULTS_DIR, 'vocab.csv'), 'a', encoding='utf-8') as f:
+            f.write('%d\t%f\n'%(vocab_size, acc))
+        
+
+def analyze_category():
+    features_train, labels_train = load_sift_features(TRAIN_DIR)
+    cluster, clf = sift_cluster_classifer(features_train, labels_train)
+    
+    categories = os.listdir(TRAIN_DIR)
+    categories = [cat.lower() for cat in categories]
+
+    features, labels = load_sift_features(TEST_DIR)
+    acc, y_pred = evaluate_sift(features, labels, cluster, clf)
+
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
+    with open(os.path.join(RESULTS_DIR, 'category.csv'), 'w', encoding='utf-8') as f:
+        f.write('Category\tNumber\tAccuracy\n')
+        f.write('%s\t%d\t%f\n'%('Total', labels.shape[0], acc))
+    
+    for index, cat in enumerate(categories):
+        correct = (y_pred == labels)
+        correct_cat = correct[labels == index]
+        number = correct_cat.shape[0]
+        acc = correct_cat.sum() / number
+        with open(os.path.join(RESULTS_DIR, 'category.csv'), 'a', encoding='utf-8') as f:
+            f.write('%s\t%d\t%f\n'%(cat, number, acc))
+
+
+def analyze_tiny():
+    clf = tiny_classifier()
+    acc, y_pred, labels= evaluate_tiny(clf)
+    
+    categories = os.listdir(TRAIN_DIR)
+    categories = [cat.lower() for cat in categories]
+
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
+    with open(os.path.join(RESULTS_DIR, 'category_tiny.csv'), 'w', encoding='utf-8') as f:
+        f.write('Category\tNumber\tAccuracy\n')
+        f.write('%s\t%d\t%f\n'%('Total', labels.shape[0], acc))
+    
+    for index, cat in enumerate(categories):
+        correct = (y_pred == labels)
+        correct_cat = correct[labels == index]
+        number = correct_cat.shape[0]
+        acc = correct_cat.sum() / number
+        with open(os.path.join(RESULTS_DIR, 'category_tiny.csv'), 'a', encoding='utf-8') as f:
+            f.write('%s\t%d\t%f\n'%(cat, number, acc))
+
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='select method: sift or tiny')
+    parser = argparse.ArgumentParser(description='select METHOD: sift or tiny or select analyze: vocab or category')
     parser.add_argument('--method', type=str, default='sift')
+    parser.add_argument('--analyze', type=str, default='null')
 
     args = parser.parse_args()
+    
+    if args.analyze == 'vocab':
+        analyze_vocabsize()
+        exit()
+    elif args.analyze == 'category':
+        analyze_category()
+        exit()
+    elif args.analyze == 'tiny':
+        analyze_tiny()
+        exit()
+    elif args.analyze != 'null':
+        raise AttributeError('%s is not a valid analyze target, only support vocab or category.'%args.analyze)
 
+    # Only if do no analysis, run below codes
     if args.method.lower() == 'tiny':
         clf = tiny_classifier()
-        acc = evaluate_tiny(clf)
+        acc, _, _ = evaluate_tiny(clf)
         print('\n\nTiny images method accuracy: %f'%acc)
     elif args.method.lower() == 'sift':
-        cluster, clf = sift_cluster_classifer()
-        acc = evaluate_sift(cluster, clf)
+        features, labels = load_sift_features(TRAIN_DIR)
+        cluster, clf = sift_cluster_classifer(features, labels)
+        features_test, labels_test = load_sift_features(TEST_DIR)
+        acc, _ = evaluate_sift(features_test, labels_test, cluster, clf)
         print('\n\nSIFT bag of words method accuracy: %f'%acc)
     else:
         raise AttributeError('%s is not a valid method, only support sift or tiny.'%args.method)
